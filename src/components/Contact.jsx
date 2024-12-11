d56753bcce357e9b98b012fe5205b9e3e32a5b95
The White Hat KE <austinkings30@gmail.com>
2024-12-11 23:48:21 +0300

    Sending emails from contact form via emailjs

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,16 +1,74 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { contacts } from '../constants';
 import CLIMockup from './CLIMockup';
+import emailjs from "@emailjs/browser";
+
 const Contact = () => {
+    const [formData, setFormData] = useState({
+        name:"",
+        email:"",
+        message:"",
+        subject:"",
+    });
+    const [sending,setSending]=useState(false);
+
     useEffect(() => {
         AOS.init({ duration: 300 });
     }, []);
 	const cliCommands = [
 		'git clone https://github.com/AustinKingOry/portfolio.git',
 	];
+    const sendEmail = (e) =>{
+        e.preventDefault();
+        setSending(true);
+        try {
+            const serviceId = import.meta.env.VITE_EMAILJS_SERVICE_ID;
+            const templateId = import.meta.env.VITE_EMAILJS_TEMPLATE_ID;
+            const publicKey = import.meta.env.VITE_EMAILJS_PUBLIC_KEY;
+
+            if(serviceId && templateId && publicKey){
+                const templateParams = {
+                    name:formData.name,
+                    email : formData.email,
+                    subject : formData.subject,
+                    message : formData.message,
+                    receiver_name : "Austin",
+                }
+    
+                emailjs.send(serviceId, templateId, templateParams, publicKey).then(
+                    (response)=>{
+                        console.log("Email sent successfully: ", response);
+                        resetForm();
+                    }
+                ).catch ((error) => {
+                    console.error(`Failed to send email: ${error}`);
+                });
+            } else {
+                console.warning("Please provide valid email service credentials.");
+                return;
+            }
+            
+        } catch (error) {
+            console.error(`Unexpected error occurred: ${error.message}`);
+        } finally {
+            setSending(false);
+        }
+    }
+    const handleChange = (e)=>{
+        const {name, value}=e.target;
+        setFormData({...formData,[name]:value})
+    }
+    const resetForm = ()=>{
+        setFormData({
+            name:"",
+            email:"",
+            message:"",
+            subject:"",
+        });
+    }
   return (
     <section id="contact" className="py-16 max-w-7xl mx-auto max-sm:mx-6 max-lg:px-6 max-sm:px-0">
         <div className="container mx-auto">
@@ -36,25 +94,25 @@ const Contact = () => {
                         ))}
                     </div>                    
                 </div>
-                <form action="submit_form.php" method="post" className="bg-gray-50 p-6 rounded-lg shadow md:mt-8">
+                <form action="" onSubmit={sendEmail} method="post" className="bg-gray-50 p-6 rounded-lg shadow md:mt-8">
                     <p className="mb-2 self-stretch text-gray-600 text-base font-normal font-['Work Sans'] leading-normal">Would you like to discuss an idea you have? Or to hire me? Get in touch with me instantly by filling in this form.</p>
                     <div className="relative z-0 w-full mb-5 group">
-                        <input type="text" name="name" id="name" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+                        <input type="text" name="name" id="name" value={formData.name} onChange={handleChange} className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
                         <label htmlFor="name" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Name</label>
                     </div>
                     <div className="relative z-0 w-full mb-5 group">
-                        <input type="email" name="email" id="email" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+                        <input type="email" name="email" id="email" value={formData.email} onChange={handleChange} className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
                         <label htmlFor="email" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Email address</label>
                     </div>
                     <div className="relative z-0 w-full mb-5 group">
-                        <input type="text" name="subject" id="subject" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+                        <input type="text" name="subject" id="subject" value={formData.subject} onChange={handleChange} className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
                         <label htmlFor="subject" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Subject</label>
                     </div>
                     <div className="relative z-0 w-full mb-5 group">
-                        <textarea name="message" id="message" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required></textarea>
+                        <textarea name="message" id="message" value={formData.message} onChange={handleChange} className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required></textarea>
                         <label htmlFor="message" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Message</label>
                     </div>
-                    <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Send</button>
+                    <button type="submit" disabled={sending} className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">{sending?"Sending":"Send"}</button>
                 </form>
             </div>
         </div>
